022856f9624c0d6cf9093d29456a885c9966ab7d
aururn <tr831042@gmail.com>
2024-07-04 15:04:40 +0900

    0704

--- python/test.py
+++ python/test.py
@@ -1,11 +1,10 @@
 import cv2
 
 # 画像読み込み
-path = "test.jpg"
+path = r"C:\Users\Aurum\Documents\img\test.jpg"
 img = cv2.imread(path)
-print("sss")
-#re_img = cv2.resize(img, dsize=(700, 700))
 
+#re_img = cv2.resize(img, dsize=(700, 700))
 cv2.imshow("pic",img)
 
 # グレイスケール
@@ -13,7 +12,6 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # 白黒に変換
 ret, thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU)
-cv2.imwrite("binary.png", thresh)
 
 #閾値がいくつになったか確認
 print("ret1: {}".format(ret))
@@ -21,8 +19,7 @@ print("ret1: {}".format(ret))
 
 # 輪郭検出
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
-cv2.imwrite("contour.png", img)
+cv2.drawContours(img, contours, -1, (255, 0, 0), 2)
 
 cv2.imshow("gray",img_gray)
 cv2.imshow("binary",thresh)
